74283311c9aa74f9be800e65fa389069f7e177ae
Diomar Majano <http.diomar@gmail.com>
2025-03-21 12:02:37 -0300

    Creacion de proyecto-microservicio

---
+++ src/main/java/pelis/cloud/peliscloud/PeliscloudApplication.java
@@ -0,0 +1,13 @@
+package pelis.cloud.peliscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliscloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliscloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pelis/cloud/peliscloud/PeliscloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pelis.cloud.peliscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliscloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
